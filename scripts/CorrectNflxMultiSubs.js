1dd55ff256bbbd8245188c4cea274b5032297a40
sean-root <107789932+sean-root@users.noreply.github.com>
2023-12-06 12:50:33 +0800

    Update CorrectNflxMultiSubs.js

--- scripts/CorrectNflxMultiSubs.js
+++ scripts/CorrectNflxMultiSubs.js
@@ -1,9 +1,18 @@
 // ==UserScript==
-// @name        纠正 NflxMultiSubs 字幕重叠的问题
-// @include     https://netflix.com/*
-// @include     https://www.netflix.com/*
-// @description 纠正 NflxMultiSubs 字幕重叠的问题
-// @author      Sean
+// @name                 纠正 NflxMultiSubs 字幕重叠的问题
+// @name:zh-CN           纠正 NflxMultiSubs 字幕重叠的问题
+// @namespace            http://tampermonkey.net/
+// @version              1.0.0
+// @description          Play Netflix UHD content on any screen resolution
+// @description:zh-CN    纠正 NflxMultiSubs 字幕重叠的问题
+// @author               TGSAN
+// @match                https://www.netflix.com/*
+// @icon                 https://www.google.com/s2/favicons?sz=64&domain=netflix.com
+// @run-at               document-start
+// @grant                unsafeWindow
+// @grant                GM_registerMenuCommand
+// @downloadURL https://github.com/sean-root/public/raw/main/scripts/CorrectNflxMultiSubs.js
+// @updateURL https://github.com/sean-root/public/raw/main/scripts/CorrectNflxMultiSubs.js
 // ==/UserScript==
 
 (function () {
